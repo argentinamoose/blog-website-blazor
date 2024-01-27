c0723310a58ad8ed02185ce41daff33498af4a61
argentinamoose <argentinamoose@gmail.com>
2024-01-26 18:34:08 -0600

    template-add

---
+++ blog-website-blazor/blog-website-blazor/Program.cs
@@ -0,0 +1,35 @@
+using blog_website_blazor.Client.Pages;
+using blog_website_blazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(blog_website_blazor.Client._Imports).Assembly);
+
+app.Run();
